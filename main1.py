d2c4c7755ca34beb4eadd27a3418cf88e4e11040
Piosza <piosza@tlen.pl>
2023-05-28 15:57:15 +0200

    game engine  update

--- main1.py
+++ main1.py
@@ -48,25 +48,40 @@ def roll_the_dice_decision(mynumbers, computer_numbers):
 
 def calculation_who_win(mynumbers, computer_numbers):
     human_score = 1
-    computer_score = 1
+
     core_point = 1
     a = mynumbers[core_point - 1]
     b = mynumbers[core_point]
     c = mynumbers[core_point + 1]
     d = mynumbers[core_point + 2]
     e = mynumbers[core_point + 3]
-
-    ac = computer_numbers[core_point - 1]
-    bc = computer_numbers[core_point]
-    cc = computer_numbers[core_point + 1]
-    dc = computer_numbers[core_point + 2]
-    ec = computer_numbers[core_point + 3]
+    computer_score = 1
+    core_point_c = 1
+    ax = computer_numbers[core_point_c - 1]
+    bx = computer_numbers[core_point_c]
+    cx = computer_numbers[core_point_c + 1]
+    dx = computer_numbers[core_point_c + 2]
+    ex = computer_numbers[core_point_c + 3]
     for i in range(1):
-        if a == b and b != c and b != d and b != e or b == c or c == d or d == e:
+        if a == b and b != c:
             human_score += 1
-        if a == b and c == d and b != e or b == c and d == e and a != b:
+        if b == c and c != d:
+            human_score += 1
+        if c == d and d != e:
+            human_score += 1
+        if d == e:
+            human_score += 1
+        if a == b and c == d:
             human_score += 2
-        if a == b == c and c != d and c != e or c == d == e and e != a and e != b:
+        if b == c and d == e:
+            human_score += 2
+        if a == b and d == e:
+            human_score += 2
+        if a == b == c:
+            human_score += 3
+        if c == d == e:
+            human_score += 3
+        if b == c == d:
             human_score += 3
         if a == 1 and b == 2 and c == 3 and d == 4 and c == 5 and e == 6:
             human_score = +4
@@ -78,22 +93,36 @@ def calculation_who_win(mynumbers, computer_numbers):
             human_score += 7
         if a == b == c == d == e:
             human_score += 8
-
-        if ac == bc:
+    for i in range(1):
+        if ax == bx and bx != cx:
             computer_score += 1
-        if ac == bc and cc == dc:
+        if bx == cx and cx != dx:
+            computer_score += 1
+        if cx == dx and dx != ex:
+            computer_score += 1
+        if dx == ex:
+            computer_score += 1
+        if ax == bx and cx == dx:
             computer_score += 2
-        if ac == bc == cc or cc == dc == e:
+        if bx == cx and dx == ex:
+            computer_score += 2
+        if ax == bx and dx == ex:
+            computer_score += 2
+        if ax == bx == cx:
             computer_score += 3
-        if ac == 1 and bc == 2 and cc == 3 and dc == 4 and cc == 5:
-            computer_score += 4
-        if ac == 2 and bc == 3 and cc == 4 and dc == 5 and cc == 6:
+        if bx == cx == dx:
+            computer_score += 3
+        if cx == dx == ex:
+            computer_score += 3
+        if ax == 1 and bx == 2 and cx == 3 and dx == 4 and cx == 5 and ex == 6:
+            computer_score = +4
+        if ax == 2 and bx == 3 and cx == 4 and dx == 5 and cx == 6:
             computer_score += 5
-        if ac == bc == cc and dc == ec or ac == bc and cc == dc == ec:
+        if ax == bx == cx and dx == ex or ax == bx and cx == dx == ex:
             computer_score += 6
-        if ac == bc == cc == dc or bc == cc == dc == ec:
+        if ax == bx == cx == dx or bx == cx == dx == ex:
             computer_score += 7
-        if ac == bc == cc == dc == ec:
+        if ax == bx == cx == dx == ex:
             computer_score += 8
         print(human_score)
         print(computer_score)
